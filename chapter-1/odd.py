41946bdd601ed36f235eacfdf6b89dde07610f2e
Patrick Bruso <pkbruso@gmail.com>
2019-04-11 21:49:44 -0700

    added comments to odd program

--- chapter-1/odd.py
+++ chapter-1/odd.py
@@ -1,11 +1,16 @@
-from datetime import datetime
+from datetime import datetime # imports datetime function
 
+#creating an array of odd numbers for 1-60 (seconds)
 odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37,
         39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
 
+#set variable right_this_minute to equal datetime today function
+#to the minute.  Will pull minute of current time
 right_this_minute = datetime.today().minute
 
+#if function to print statement if the time is found in the array
+#else statement if time is not found in array
 if right_this_minute in odds:
     print("This minute seems a little odd.")
 else:
