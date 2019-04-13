f116bc2cdaa17de3323001232a28019be8c393da
Patrick Bruso <pkbruso@gmail.com>
2019-04-12 21:01:58 -0700

    added comments to beersong program

--- chapter-1/beersong.py
+++ chapter-1/beersong.py
@@ -1,11 +1,16 @@
 #program to implement classic beer song "99 bottles of beer"
 
+#set variable word for bottle(s)
 word = "bottles"
+
+#for loop from 99 to 0
 for beer_num in range(99, 0, -1):
     print(beer_num, word, "of beer on the wall.")
     print(beer_num, word, "of beer.")
     print("Take one down.")
     print("Pass it around.")
+
+    #if/else for when beer gets to 1/0
     if beer_num == 1:
         print("No more bottles of beer on the wall.")
     else:
