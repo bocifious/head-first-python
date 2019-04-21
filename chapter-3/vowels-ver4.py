49a0ed6c15553e68548f01a8799c3563b131eda7
Patrick Bruso <pkbruso@gmail.com>
2019-04-20 17:02:43 -0700

    added comments to vowels-ver4

--- chapter-3/vowels-ver4.py
+++ chapter-3/vowels-ver4.py
@@ -1,6 +1,10 @@
+#create a list containing vowels
 vowels = ['a', 'e', 'i', 'o', 'u']
+
+#assign user's input to a variable
 word = input("Provide a word to search for vowels: ")
 
+#create empty dict for the vowels
 found = {}
 found['a'] = 0
 found['e'] = 0
@@ -8,9 +12,11 @@ found['i'] = 0
 found['o'] = 0
 found['u'] = 0
 
+#for loop to add instances of vowels to dict
 for letter in word:
     if letter in vowels:
         found[letter] += 1
 
+#printing the sorted dict of vowels
 for k, v in sorted(found.items()):
     print(k, 'was found', v, 'time(s).')
