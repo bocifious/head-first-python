3d58cf9fa401b8dc27dd8c14a88e9e348ca8dfc9
Patrick Bruso <pkbruso@gmail.com>
2019-05-21 21:04:19 -0700

    added mystery program

---
+++ chapter-4/mystery.py
@@ -0,0 +1,11 @@
+#function to present a call-by-value argument
+def double(arg):
+    print('Before: ', arg)
+    arg = arg * 2
+    print('After: ', arg)
+
+#function to present a call-by-reference argument
+def change(arg):
+    print('Before: ', arg)
+    arg.append('More data')
+    print('After: ', arg)
