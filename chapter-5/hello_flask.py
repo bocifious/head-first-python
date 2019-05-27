713289ca26d6b917a2985effa88de5ba2a1eec10
Patrick Bruso <pkbruso@gmail.com>
2019-05-27 15:57:30 -0700

    added comments to hello_flask app

--- chapter-5/hello_flask.py
+++ chapter-5/hello_flask.py
@@ -1,13 +1,16 @@
 # introduction to flask web apps
 
+# import Flask class from the flask module
 from flask import Flask
 
+# create an object type of Flask assigning it to app variable
 app = Flask(__name__)
 
-
+# function decorator and function
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
 
 
+# used to run the webapp 'app' in the web server
 app.run()
