21feccf8004a52f12cb2ff5e1b54f86a5e3e02d8
Patrick Bruso <pkbruso@gmail.com>
2019-05-26 18:27:19 -0700

    fixed mystery.py for pep8 compatibility;

--- chapter-4/mystery.py
+++ chapter-4/mystery.py
@@ -1,10 +1,14 @@
-#function to present a call-by-value argument
+# function to present a call-by-value argument
+
+
 def double(arg):
     print('Before: ', arg)
     arg = arg * 2
     print('After: ', arg)
 
-#function to present a call-by-reference argument
+# function to present a call-by-reference argument
+
+
 def change(arg):
     print('Before: ', arg)
     arg.append('More data')
