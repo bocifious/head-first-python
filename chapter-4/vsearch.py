ae6a31abfb93d141c25acf0651e1bb0a8278f97e
Patrick Bruso <pkbruso@gmail.com>
2019-05-26 18:22:11 -0700

    fixed vsearch.py for pep8 style guide

--- chapter-4/vsearch.py
+++ chapter-4/vsearch.py
@@ -1,6 +1,8 @@
-#Function to search for vowels within a supplied word
-#the ':str) -> set' is the annotation for the function
-def search_for_vowels(phrase:str) -> set:
-    #this is the vowel search code from chapter 3
+# Function to search for vowels within a supplied word
+# the ':str) -> set' is the annotation for the function
+
+
+def search_for_vowels(phrase: str) -> set:
+    # this is the vowel search code from chapter 3
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
