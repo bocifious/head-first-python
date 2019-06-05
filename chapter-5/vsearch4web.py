0967e5431763ab7ea9425ce93939cbc06d364bf7
Patrick Bruso <pkbruso@gmail.com>
2019-06-04 20:53:16 -0700

    updated code to send results to results.html

--- chapter-5/vsearch4web.py
+++ chapter-5/vsearch4web.py
@@ -17,10 +17,14 @@ def hello() -> str:
 # add new route for search4 and then function to search
 # for set within phrase by using imported module search4letters
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(phrase, letters))
+    title = 'Here are your results: '
+    results = str(search4letters(phrase, letters))
+    return render_template(
+        'results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,
+    )
 
 
 # function to return rendered HTML
