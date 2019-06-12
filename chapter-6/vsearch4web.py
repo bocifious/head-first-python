47dceed55ff7c0194e14c827d1d1865d24dd1e40
Patrick Bruso <pkbruso@gmail.com>
2019-06-11 18:30:24 -0700

    added delimiter to log

--- chapter-6/vsearch4web.py
+++ chapter-6/vsearch4web.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 def log_request(req: 'flask_request', res: str) -> None:
     """function to log results of search4letters function"""
     with open('vsearch.log', 'a') as log:
-        print(str(dir(req)), res, file=log)
+        print(req.form, file=log, end='|')
+        print(req.remote_addr, file=log, end='|')
+        print(req.user_agent, file=log, end='|')
+        print(res, file=log, end='|')
 
 # add new route for search4 and then function to search
 # for set within phrase by using imported module search4letters
