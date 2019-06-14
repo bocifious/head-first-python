d93b986a0477ef2c8ade9d37a353308165175c39
Patrick Bruso <pkbruso@gmail.com>
2019-06-14 10:52:54 -0700

    updated viewlog function to change log to list

--- chapter-6/vsearch4web.py
+++ chapter-6/vsearch4web.py
@@ -46,9 +46,13 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> str:
+    contents = []
     with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    return str(contents)
 
 
 # used to run the webapp 'app' in the web server
