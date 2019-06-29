59f335f29d83b5b020c8a060dc3f89fb8ca37813
Patrick Bruso <pkbruso@gmail.com>
2019-06-28 21:27:55 -0700

    added mysql connection to log_request

--- chapter-7/vsearch4web.py
+++ chapter-7/vsearch4web.py
@@ -11,13 +11,25 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     """function to log results of search4letters function"""
-    with open('vsearch.log', 'a') as log:
-        print(
-            req.form,
-            req.remote_addr,
-            req.user_agent,
-            res,
-            file=log, sep='|')
+    dbconfig = {'host': '127.0.0.1',
+                'user': 'vsearch',
+                'password': 'vsearchpasswd',
+                'database': 'vsearchlogDB', }
+    import mysql.connector
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log
+              (phrase, letters, ip, browser_string, results)
+              values
+              (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL,    (req.form['phrase'],
+                            req.form['letters'],
+                            req.remote_addr,
+                            req.user_agent.browser,
+                            res, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 # add new route for search4 and then function to search
 # for set within phrase by using imported module search4letters
