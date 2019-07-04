fe4d1c94aea153831a9cef249671b32c0a108891
Patrick Bruso <pkbruso@gmail.com>
2019-07-04 15:27:51 -0700

    added dunder repr method to return val as string

--- chapter-8/countfromby.py
+++ chapter-8/countfromby.py
@@ -4,8 +4,13 @@ class CountFromBy:
     def __init__(self, v: int, i: int) -> None:
         self.val = v
         self.incr = i
+        
     # increase method to add increment to value
     def increase(self) -> None:
         self.val += self.incr
+
+    # convert interger value of val to string
+    def __repr__(self) -> str:
+        return str(self.val)
 
 
