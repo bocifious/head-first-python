7188d79c615f12d15aa9113da871b2164a211f78
Patrick Bruso <pkbruso@gmail.com>
2019-07-05 21:21:02 -0700

    fixed code for pep8 formatting

--- chapter-8/countfromby.py
+++ chapter-8/countfromby.py
@@ -1,10 +1,10 @@
 # creating a class to increase a value by an increment
 class CountFromBy:
     # dunder init method for initailization of value and increment
-    def __init__(self, v: int=0, i: int=1) -> None:
+    def __init__(self, v: int = 0, i: int = 1) -> None:
         self.val = v
         self.incr = i
-        
+
     # increase method to add increment to value
     def increase(self) -> None:
         self.val += self.incr
@@ -12,4 +12,3 @@ class CountFromBy:
     # convert interger value of val to string
     def __repr__(self) -> str:
         return str(self.val)
-
